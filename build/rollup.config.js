adc6aa5cca5f4ff83ba3870195a20f5cc3f85111
Josef Mueller <j.mueller5021@gmail.com>
2021-09-11 14:48:17 +0200

    :zap: rollup.config.js: added `ol` as external dependency.

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -72,6 +72,7 @@ const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
   'vue',
+  'ol'
 ];
 
 // UMD/IIFE shared settings: output.globals
